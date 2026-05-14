393f7ccf2eb940d011e1244228e5818ee86101e6
Vivaan <fowlstar1@gmail.com>
2026-05-14 23:02:07 +0530

    added vertex loop and basic noise math

--- main.js
+++ main.js
@@ -21,13 +21,27 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
-
-
 const noise2D = createNoise2D();
 
 const island_geo = new THREE.PlaneGeometry(2000, 2000, 256, 256);
 island_geo.rotateX(-Math.PI / 2);
 const pos_attr = island_geo.attributes.position;
+
+for (let i = 0; i < pos_attr.count; i++) {
+     const x = pos_attr.getX(i);
+     const z = pos_attr.getZ(i);
+     
+
+ let h_val = noise2D(x * 0.005, z * 0.005) * 150;
+        h_val += noise2D(x * 0.01, z * 0.01) * 50;
+        h_val += noise2D(x * 0.05, z * 0.05) * 10;
+
+
+        const dist = Math.sqrt(x * x + z * z);
+        h_val -= Math.max(0, dist * 0.15);
+
+        pos_attr.setY(i, h_val);
+}
 
 function animate() {
     requestAnimationFrame(animate);
